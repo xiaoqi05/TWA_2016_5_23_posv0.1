f05ce8caf45e96dcda973d03e5167955c3f7d47d
xiaoqi05 <772508341@qq.com>
2016-05-24 17:22:59 +0800

    refactor

--- TWA_2016_5_23_posv0.1/src/main.js
+++ TWA_2016_5_23_posv0.1/src/main.js
@@ -2,31 +2,30 @@
 
 function printInventory(inputs) {
     var cartItems = [];
-    var cartItemsBarCode = [];
+    var cartItemsBarCodes = [];
     var totalPrice = 0;
 
-    inputs.forEach(function (element) {
-        var key = element.barcode;
+    inputs.forEach(function (item) {
+        var key = item.barcode;
         if (cartItems[key]) {
             cartItems[key].num++;
             cartItems[key].sumPrice += cartItems[key].price;
         } else {
-            cartItems[element.barcode] = {
-                barcode: element.barcode,
-                name: element.name,
-                unit: element.unit,
-                price: element.price,
-                sumPrice: element.price,
+            cartItems[item.barcode] = {
+                barcode: item.barcode,
+                name: item.name,
+                unit: item.unit,
+                price: item.price,
+                sumPrice: item.price,
                 num: 1
             };
-            cartItemsBarCode.push(element.barcode);
+            cartItemsBarCodes.push(item.barcode);
 
         }
         totalPrice += cartItems[key].price;
 
     });
-    
-    var resultString = spliceInventoryResultString(cartItems,cartItemsBarCode,totalPrice);
+    var resultString = spliceInventoryResultString(cartItems, cartItemsBarCodes, totalPrice);
     console.log(resultString);
 }
 
@@ -38,13 +37,18 @@ function spliceInventoryResultString(cartItems, cartItemsBarCode, totalPrice) {
     cartItemsBarCode.forEach(function (item) {
         resultContent += getSingleCartItemInventoryString(cartItems, item);
     });
-    return resultTitle + resultContent + resultDivile + "总计：" + totalPrice.toFixed(2) + "(元)\n" + resultStar;
+    var resultTotalPrice = "总计：" + formatPrice(totalPrice) + "(元)\n";
+    return resultTitle + resultContent + resultDivile + resultTotalPrice + resultStar;
 }
 
 function getSingleCartItemInventoryString(cartItems, key) {
     var singleCartItemResult = "";
-    singleCartItemResult = "名称：" + cartItems[key].name + "，数量：" + cartItems[key].num + "瓶，单价：" + cartItems[key].price.toFixed(2) + "(元)，小计：" + (cartItems[key].num * cartItems[key].price).toFixed(2) + "(元)\n";
+    singleCartItemResult = "名称：" + cartItems[key].name + "，数量：" + cartItems[key].num + "瓶，单价：" + formatPrice(cartItems[key].price) + "(元)，小计：" + formatPrice(cartItems[key].num * cartItems[key].price) + "(元)\n";
     return singleCartItemResult;
+}
+
+function formatPrice(price) {
+    return price.toFixed(2);
 }
 
 
